4ba1233ae4636b88a3ee9f859c3aa1afc63ee71c
haycon <46656475+haycon@users.noreply.github.com>
2021-01-16 15:48:21 +0100

    Init

---
+++ script.js
@@ -0,0 +1,19 @@
+const Board = () => {
+  const board = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ];
+};
+
+const Game = (player1, player2) => {
+  const getPlayer1 = () => player1;
+  const getPlayer2 = () => player2;
+  const win = () => {};
+};
+
+const Player = (name) => {
+  const getName = () => name;
+
+  return getName;
+};
